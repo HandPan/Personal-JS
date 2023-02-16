1b33d6afcf116b442a8265dced3e1e232bef4f30
jacobsr6 <jacobsr6@wwu.edu>
2023-02-15 23:17:59 -0800

    Modified cells to objects for data storage

--- Sudoku/sudoku.js
+++ Sudoku/sudoku.js
@@ -11,6 +11,8 @@ function testJS() {
     document.getElementById("testButton").innerHTML = "It Worked!";
 }
 
+let squares = new Array(81);
+
 function makeGrid() {
     
     const sudokuGrid = document.getElementById("sudokuGrid");
@@ -20,22 +22,40 @@ function makeGrid() {
         let largeCell = document.createElement("div");
         sudokuGrid.appendChild(largeCell).className = "sub-grid-item";
     }
-    console.log(sudokuGrid);
+    // console.log(sudokuGrid);
 
     for (let h = 0; h < 9; h+=3) {
         for (let l = 0; l < 3; l++) {
             for (let j = 0; j < 3; j++) {
                 let largeCell = sudokuGrid.getElementsByClassName("sub-grid-item")[j+h];
-                console.log(largeCell);
+                // console.log(largeCell);
                 for (let k = 0; k < 3; k++) {
+                    let cellDisplay = document.createElement("div");
+                    let cell = {
+                        cellDisplay : cellDisplay,
+                        id : total,
+                        cellValue : 0,
+                        entropy : 9
+                    };
+                    squares[total] = cell;
                     total++;
-                    let cell = document.createElement("div");
-                    cell.innerText = (total);
-                    largeCell.appendChild(cell).className = "grid-item";
+                    // cellDisplay.innerText = (total);
+                    largeCell.appendChild(cellDisplay).className = "grid-item";
                 }
             }
         }
     }
+
+    // for (let i = 0; i < 81; i++) {
+    //     squares[i].cellValue = 5;
+    // }
+
+    // for (let i = 0; i < 81; i++) {
+    //     let curSquare = squares[i];
+    //     curSquare.cellDisplay.innerText = curSquare.cellValue;
+    // }
+
+    console.log(squares);
 
     document.getElementById("testButton").innerHTML = "It Worked!";
 }
