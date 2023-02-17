ac947c160c5eb6487287fe354567bbfec65df3ae
jacobsr6 <jacobsr6@wwu.edu>
2023-02-16 18:14:15 -0800

    Constructed temporary test imput method

--- Sudoku/sudoku.js
+++ Sudoku/sudoku.js
@@ -12,50 +12,87 @@ function testJS() {
 }
 
 let squares = new Array(81);
+let startCode = new Array(81);
+let gridActive = false;
+
+function submitted() {
+    makeGrid();
+    codeParser();
+}
 
 function makeGrid() {
     
-    const sudokuGrid = document.getElementById("sudokuGrid");
-
-    let total = 0;
-    for (let i = 0; i < 9; i++) {
-        let largeCell = document.createElement("div");
-        sudokuGrid.appendChild(largeCell).className = "sub-grid-item";
-    }
-    // console.log(sudokuGrid);
-
-    for (let h = 0; h < 9; h+=3) {
-        for (let l = 0; l < 3; l++) {
-            for (let j = 0; j < 3; j++) {
-                let largeCell = sudokuGrid.getElementsByClassName("sub-grid-item")[j+h];
-                // console.log(largeCell);
-                for (let k = 0; k < 3; k++) {
-                    let cellDisplay = document.createElement("div");
-                    let cell = {
-                        cellDisplay : cellDisplay,
-                        id : total,
-                        cellValue : 0,
-                        entropy : 9
-                    };
-                    squares[total] = cell;
-                    total++;
-                    // cellDisplay.innerText = (total);
-                    largeCell.appendChild(cellDisplay).className = "grid-item";
+    if (!gridActive) {
+        const sudokuGrid = document.getElementById("sudokuGrid");
+    
+        let total = 0;
+        for (let i = 0; i < 9; i++) {
+            let largeCell = document.createElement("div");
+            sudokuGrid.appendChild(largeCell).className = "sub-grid-item";
+        }
+        console.log(sudokuGrid);
+    
+        for (let h = 0; h < 9; h+=3) {
+            for (let l = 0; l < 3; l++) {
+                for (let j = 0; j < 3; j++) {
+                    let largeCell = sudokuGrid.getElementsByClassName("sub-grid-item")[j+h];
+                    // console.log(largeCell);
+                    for (let k = 0; k < 3; k++) {
+                        let cellDisplay = document.createElement("div");
+                        let cell = {
+                            cellDisplay : cellDisplay,
+                            id : total,
+                            cellValue : 0,
+                            entropy : 9
+                        };
+                        squares[total] = cell;
+                        total++;
+                        // cellDisplay.innerText = (total);
+                        largeCell.appendChild(cellDisplay).className = "grid-item";
+                    }
                 }
             }
         }
+
+        gridActive = true;
     }
-
-    // for (let i = 0; i < 81; i++) {
-    //     squares[i].cellValue = 5;
-    // }
-
-    // for (let i = 0; i < 81; i++) {
-    //     let curSquare = squares[i];
-    //     curSquare.cellDisplay.innerText = curSquare.cellValue;
-    // }
-
     console.log(squares);
+}
 
-    document.getElementById("testButton").innerHTML = "It Worked!";
+function codeParser() {
+    let codeString = document.getElementById("inputCodeBox").value;
+    // console.log(codeString.length);
+    if (codeString.length == 81) {
+        let codeTemp = codeString.match(/(\d)/g);
+        // console.log(codeTemp);
+        if (codeTemp.length == 81) {
+            startCode = codeTemp;
+            inputFromCode();
+        }
+    }
+}
+
+function inputFromCode() {
+    // Clear any previous data
+    squares.forEach(element => {
+        element.cellValue = 0;
+        element.cellDisplay.innerText = "";
+        element.entropy = 9;
+    });
+
+    // Fill grid with input data
+    let i = 0;
+    let cur;
+    if (startCode) {
+        console.log(startCode);
+        startCode.forEach(element => {
+            cur = squares[i];
+            cur.cellValue = element;
+            if (element != 0) {
+                cur.cellDisplay.innerText = element;
+                cur.entropy = 1;
+            }
+            i++;
+        });
+    }
 }
