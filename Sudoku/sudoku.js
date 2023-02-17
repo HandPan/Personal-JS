804cec1555e34cdb7829d6af2fa4d5cb836c69c2
jacobsr6 <jacobsr6@wwu.edu>
2023-02-17 01:38:34 -0800

    Completed first pass code along with an iterative test solver

--- Sudoku/sudoku.js
+++ Sudoku/sudoku.js
@@ -39,15 +39,23 @@ function makeGrid() {
                     // console.log(largeCell);
                     for (let k = 0; k < 3; k++) {
                         let cellDisplay = document.createElement("div");
+                        let possibilities = [1, 2, 3, 4, 5, 6, 7, 8, 9];
                         let cell = {
                             cellDisplay : cellDisplay,
                             id : total,
                             cellValue : 0,
-                            entropy : 9
+                            entropy : 9,
+                            possi : possibilities
                         };
                         squares[total] = cell;
                         total++;
+        
+                        // Test display values
                         // cellDisplay.innerText = (total);
+                        cellDisplay.innerText = (calcColumnStart(total));
+                        // cellDisplay.innerText = (calcRowStart(total));
+                        // cellDisplay.innerText = (calcSubSquareStart(total));
+
                         largeCell.appendChild(cellDisplay).className = "grid-item";
                     }
                 }
@@ -91,8 +99,150 @@ function inputFromCode() {
             if (element != 0) {
                 cur.cellDisplay.innerText = element;
                 cur.entropy = 1;
+                for (let j = 0; j < 9; j++) {
+                    if (cur.possi[j] != element) {
+                        cur.possi[j] = 0;
+                    }
+                }
             }
             i++;
         });
     }
+}
+
+function calcInitialEntropy() {
+    squares.forEach(square => {
+        if (square.entropy == 1) {
+
+            // Rows
+            let i = calcRowStart(square.id+1)-1;
+            let end_i = i + 9;
+            while (i < end_i) {
+                let curSquare = squares[i];
+                if (curSquare.entropy != 1) {
+                    if (curSquare.possi[square.cellValue-1]) {
+                        curSquare.possi[square.cellValue-1] = 0;
+                        curSquare.entropy--;
+
+                        // Temp display entropy
+                        curSquare.cellDisplay.innerText = curSquare.entropy;
+                        curSquare.cellDisplay.style.color = "red";
+
+                    }
+                }
+
+                // Temp for test solver
+                if (curSquare.entropy == 1 && curSquare.cellValue == 0) {
+                    let c = 0;
+                    while (!curSquare.possi[c]) {
+                        c++;
+                    }
+                    curSquare.cellValue = curSquare.possi[c];
+                    curSquare.cellDisplay.innerText = curSquare.cellValue;
+                    curSquare.cellDisplay.style.color = "black";
+                }
+                i++;
+            }
+
+            // Columns
+            let j = calcColumnStart(square.id+1)-1;
+            // console.log(j);
+            let end_j = j + 72;
+            while (j < end_j) {
+                let curSquare = squares[j];
+                if (curSquare.entropy != 1) {
+                    if (curSquare.possi[square.cellValue-1]) {
+                        curSquare.possi[square.cellValue-1] = 0;
+                        curSquare.entropy--;
+
+                        // Temp display entropy
+                        curSquare.cellDisplay.innerText = curSquare.entropy;
+                        curSquare.cellDisplay.style.color = "red";
+                    }
+                }
+
+                // Temp for test solver
+                if (curSquare.entropy == 1 && curSquare.cellValue == 0) {
+                    let c = 0;
+                    while (!curSquare.possi[c]) {
+                        c++;
+                    }
+                    curSquare.cellValue = curSquare.possi[c];
+                    curSquare.cellDisplay.innerText = curSquare.cellValue;
+                    curSquare.cellDisplay.style.color = "black";
+                }
+                j += 9;
+            }
+
+
+            // SubSquares
+            let k = calcSubSquareStart(square.id+1)-1;
+            // console.log(k);
+            let end_k = k + 21;
+            while (k < end_k) {
+                // console.log(k);
+                let curSquare = squares[k];
+                if (curSquare.entropy != 1) {
+                    if (curSquare.possi[square.cellValue-1]) {
+                        curSquare.possi[square.cellValue-1] = 0;
+                        curSquare.entropy--;
+
+                        // Temp display entropy
+                        curSquare.cellDisplay.innerText = curSquare.entropy;
+                        curSquare.cellDisplay.style.color = "red";
+                    }
+                }
+
+                // Temp for test solver
+                if (curSquare.entropy == 1 && curSquare.cellValue == 0) {
+                    let c = 0;
+                    while (!curSquare.possi[c]) {
+                        c++;
+                    }
+                    curSquare.cellValue = curSquare.possi[c];
+                    curSquare.cellDisplay.innerText = curSquare.cellValue;
+                    curSquare.cellDisplay.style.color = "black";
+                }
+
+                
+                if ((k+1)%3 == 0) {
+                    k += 7;
+                } else {
+                    k++;
+                }
+            }
+
+        }
+    });
+}
+
+function calcRowStart(value) {
+    if (value%9) {
+        return value - (value%9) + 1;
+    } else {
+        return value - 9 + 1;
+    }
+}
+
+function calcColumnStart(value) {
+    if (value%9) {
+        return value % 9;
+    } else {
+        return 9;
+    }
+}
+
+function calcSubSquareStart(value) {
+    let subCol;
+    if (value%3) {
+        subCol = (value % 3) - 1;
+    } else {
+        subCol = 2;
+    }
+
+    let subRow = (Math.ceil(value/9)-1) % 3;
+    // return subCol;
+    // return subRow;
+
+    return value - (subRow*9) - subCol;
 }
